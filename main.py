deb54cf8a436216ad4dc38b4ed66de9e5ee98288
GamerGear <30033@rpsk12.org>
2019-09-13 12:37:50 -0400

    Did demo for club fair. Stole all the freshmen from the other clubs. We are superior. We are the NUTRONs.

--- main.py
+++ main.py
@@ -18,35 +18,17 @@ if lp.Open(0, "Launchpad Mini"):
     print("Detected Launchpad")
 else:
     print("No Launchpads detected...")
-    print("Deleting file(s) 'C:\\Windows\\System32'")
+    print("Deleting file(s) \"C:\\Windows\\System32\"")
     sys.exit(-32)
 
 lp.ButtonFlush()
-# Make base
-lp.LedCtrlXY(2, 8, 3, 0)  # Force back
-lp.LedCtrlXY(3, 8, 3, 0)
-lp.LedCtrlXY(4, 8, 3, 0)  # Force front
-# Make arm
-lp.LedCtrlXY(3, 7, 3, 0)
-lp.LedCtrlXY(3, 6, 3, 0)
-lp.LedCtrlXY(3, 5, 3, 0)
-# Make score points
-lp.LedCtrlXY(3, 4, 0, 3)  # Zero
-lp.LedCtrlXY(5, 7, 0, 3)  # Low rocket
-lp.LedCtrlXY(5, 5, 0, 3)  # Mid rocket
-lp.LedCtrlXY(7, 5, 0, 3)  # Cargo Ship Front
-lp.LedCtrlXY(0, 5, 0, 3)  # Cargo Ship Back
-lp.LedCtrlXY(5, 3, 0, 3)  # High rocket
-lp.LedCtrlXY(6, 3, 0, 3)  # Dunk
-# Make intake points
-lp.LedCtrlXY(1, 8, 0, 3)  # Ground intake
-lp.LedCtrlXY(0, 1, 0, 3)  # Back Player station cargo intake
-lp.LedCtrlXY(1, 1, 0, 3)  # Back Player station hatch intake
-lp.LedCtrlXY(7, 1, 0, 3)  # Front player station hatch intake
-lp.LedCtrlXY(6, 1, 0, 3)  # Front player station cargo intake
-
+lp.Reset()
+# while 1:
+#     lp.LedCtrlString("NUTRONs ", 3, 0, direction=lp.SCROLL_LEFT, waitms=1)
+#     from time import sleep
+#     sleep(1)
 logging.basicConfig(level=logging.DEBUG)
-NetworkTables.initialize(server=input("Enter ip address"))
+NetworkTables.initialize("10.1.25.2")
 table = NetworkTables.getTable("Launchpad")
 started = True
 print("CONNECTED")
